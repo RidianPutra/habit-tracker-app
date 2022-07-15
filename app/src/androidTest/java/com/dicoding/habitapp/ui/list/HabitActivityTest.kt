756bf8f5021a7ce8e1506bd8a533c0e8989070ca
ridianputra <ridianputra.id@gmail.com>
2022-07-15 09:37:46 +0700

    first commit

---
+++ app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
@@ -0,0 +1,25 @@
+package com.dicoding.habitapp.ui.list
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.dicoding.habitapp.ui.add.AddHabitActivity
+import com.dicoding.habitapp.R
+import org.junit.Rule
+import org.junit.Test
+
+//TODO 16 : Write UI test to validate when user tap Add Habit (+), the AddHabitActivity displayed
+class HabitActivityTest {
+    @get:Rule
+    val activity = ActivityScenarioRule(HabitListActivity::class.java)
+
+    @Test
+    fun loadAddHabitActivity_Success() {
+        Intents.init()
+        Espresso.onView(withId(R.id.fab)).perform(ViewActions.click())
+        Intents.intended(hasComponent(AddHabitActivity::class.java.name))
+    }
+}
---
+++ app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
@@ -0,0 +1,72 @@
+package com.dicoding.habitapp.notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.TaskStackBuilder
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import androidx.preference.PreferenceManager
+import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.detail.DetailHabitActivity
+import com.dicoding.habitapp.utils.HABIT_ID
+import com.dicoding.habitapp.utils.HABIT_TITLE
+import com.dicoding.habitapp.utils.NOTIFICATION_CHANNEL_ID
+
+class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+
+    private val habitId = inputData.getInt(HABIT_ID, 0)
+    private val habitTitle = inputData.getString(HABIT_TITLE)
+
+    private fun getPendingIntent(habitId: Int): PendingIntent? {
+        val intent = Intent(applicationContext, DetailHabitActivity::class.java).apply {
+            putExtra(HABIT_ID, habitId)
+        }
+        return TaskStackBuilder.create(applicationContext).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+    }
+
+    override fun doWork(): Result {
+        val prefManager = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val shouldNotify =
+            prefManager.getBoolean(applicationContext.getString(R.string.pref_key_notify), false)
+
+        //TODO 12 : If notification preference on, show notification with pending intent
+        if (shouldNotify) {
+            val notificationManager =
+                applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notification =
+                NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
+                    .setContentIntent(getPendingIntent(habitId))
+                    .setSmallIcon(R.drawable.ic_notifications)
+                    .setContentTitle(habitTitle)
+                    .setContentText(
+                        applicationContext.resources.getString(
+                            R.string.notify_content
+                        )
+                    )
+                    .setAutoCancel(true)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    NOTIFICATION_CHANNEL_ID,
+                    applicationContext.resources.getString(
+                        R.string.notify_channel_name
+                    ),
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+                notification.setChannelId(NOTIFICATION_CHANNEL_ID)
+                notificationManager.createNotificationChannel(channel)
+            }
+
+            notificationManager.notify(1, notification.build())
+        }
+        return Result.success()
+    }
+}
